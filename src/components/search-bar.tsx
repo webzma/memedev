769e9424c8943fdddadc28ec410c3dec69d73db2
Wilberk <ledezma.wilberk@gmail.com>
2025-03-27 20:12:37 -0400

    bg button color changed

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -37,7 +37,7 @@ export default function SearchBar() {
           type="submit"
           size="sm"
           variant="ghost"
-          className="absolute right-0 top-0 h-full px-3 text-muted-foreground hover:text-foreground code-font"
+          className="absolute right-0 top-0 h-full px-3 text-muted-foreground hover:text-foreground code-font hover:bg-primary/40!"
         >
           run()
         </Button>
